1a508c5ac3ab1a8285e9018ce2d55f2fd893c5dd
Carlos Tutiya <ctutiya@hotmail.com>
2022-10-28 09:40:59 -0700

    remove comments

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -4,11 +4,9 @@ import HeaderLogo from './HeaderLogo'
 import HeaderNav from './HeaderNav'
 
 const Header = () => {
-  // const { headerTransparent, scrolled } = useHeader()
   const { scrolled } = useHeader()
 
   return (
-    // <header className={`header-container flex justify-between items-center flex-wrap transition-all duration-300 ${scrolled ? 'fixed' : 'relative'}`}>
     <header className={`header-container ${scrolled ? 'scrolled' : ''}`}>
       <HeaderLogo />
       <HeaderNav />
--- pages/services.js
+++ pages/services.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Hero from '../components/Hero/Hero'
-// import TwoColumns from '../components/TwoColumns/TwoColumns'
 import TwoColumnsWithImage from '../components/TwoColumnsWithImage/TwoColumnsWithImage'
 import IconBox from '../components/IconBox/IconBox'
 import OurMission from '../components/OurMission/OurMission'
@@ -21,17 +20,9 @@ const Services = () => {
 
       <Hero
         customImage
-        // containerClass="pt-[50px] pb-[194px] md:pt-[85px] md:pb-[400px] lg:py-[85px] bg-[#F6F6F6] bg-contain bg-right-bottom"
-        // containerClass="pt-[50px] pb-[194px] md:pt-[85px] md:pb-[400px] lg:py-[85px] bg-[#F6F6F6] bg-cover bg-left-bottom"
         containerClass="pt-[258px] md:pt-[505px] pb-[50px] md:pb-[85px] lg:py-[85px] bg-[#F6F6F6] bg-[length:auto_208px] md:bg-[length:auto_420px] lg:bg-cover bg-right-top lg:bg-left-bottom"
         contentClass="lg:max-w-[570px]"
         content={heroContent} />
-
-      {/* <TwoColumns
-        containerClass="pb-0 flex flex-wrap lg:flex-nowrap justify-center items-center"
-        col1Class="w-full lg:w-1/2 order-last lg:order-none"
-        col2Class="w-full lg:w-1/2 pb-[30px] md:pb-[50px] lg:py-[45px] lg:px-[76px]"
-        content={expenditureAuditsContent} /> */}
 
       <TwoColumnsWithImage
         container
@@ -39,23 +30,11 @@ const Services = () => {
         columnContentClass="first"
         content={expenditureAuditsContent} />
 
-      {/* <TwoColumns
-        containerClass="py-0 flex flex-wrap lg:flex-nowrap justify-center items-center"
-        col1Class="w-full lg:w-1/2 py-[30px] md:py-[50px] lg:py-[45px] lg:px-[76px]"
-        col2Class="w-full lg:w-1/2"
-        content={allocationAuditsContent} /> */}
-
       <TwoColumnsWithImage
         container
         imageOnRight
         containerClass="py-0"
         content={allocationAuditsContent} />
-
-      {/* <TwoColumns
-        containerClass="pt-0 flex flex-wrap lg:flex-nowrap justify-center items-center"
-        col1Class="w-full lg:w-1/2 order-last lg:order-none"
-        col2Class="w-full lg:w-1/2 py-[30px] md:py-[50px] lg:py-[45px] lg:px-[76px]"
-        content={fsoaAuditsContent} /> */}
 
       <TwoColumnsWithImage
         container
