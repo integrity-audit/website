e567a119f057f2fa87c16d6676e8c1118761a8cb
Jason Wiens <jwiens@integrity-tech.ca>
2022-11-17 13:37:39 -0700

    fix file paths

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,22 +1,25 @@
-import Head from 'next/head'
-import Hero from '../components/Hero/Hero'
-import OneColumn from '../components/OneColumn/OneColumn'
-import OurTeam from '../components/OurTeam/OurTeam'
-import IconBox from '../components/IconBox/IconBox'
-import OurMission from '../components/OurMission/OurMission'
+import Head from "next/head";
+import Hero from "../components/Hero/Hero";
+import OneColumn from "../components/OneColumn/OneColumn";
+import OurTeam from "../components/OurTeam/OurTeam";
+import IconBox from "../components/IconBox/IconBox";
+import OurMission from "../components/OurMission/OurMission";
 
-import { heroContent } from '../content/AboutUs/heroContent.js'
-import { hereToServeContent } from '../content/AboutUs/hereToServeContent.js'
-import { associationsContent } from '../content/AboutUs/associationsContent.js'
-import { organizationsContent } from '../content/AboutUs/organizationsContent.js'
-import { givingBackContent } from '../content/AboutUs/givingBackContent.js'
+import { heroContent } from "../content/AboutUs/heroContent.js";
+import { hereToServeContent } from "../content/AboutUs/hereToServeContent.js";
+import { associationsContent } from "../content/AboutUs/associationsContent.js";
+import { organizationsContent } from "../content/AboutUs/organizationsContent.js";
+import { givingBackContent } from "../content/AboutUs/givingBackContent.js";
 
 const AboutUs = () => {
   return (
     <>
       <Head>
         <title>About Integrity | {process.env.NEXT_PUBLIC_SITE_NAME}</title>
-        <meta name="description" content="The Integrity Audit and Accounting team." />
+        <meta
+          name="description"
+          content="The Integrity Audit and Accounting team."
+        />
       </Head>
 
       <Hero content={heroContent} />
@@ -25,24 +28,20 @@ const AboutUs = () => {
 
       <OurTeam />
 
-      <OneColumn
-        bgColor="#F6F6F6"
-        content={associationsContent} />
+      <OneColumn bgColor="#F6F6F6" content={associationsContent} />
 
       <IconBox
         centerContent
         containerClass="max-w-[995px] gap-[50px]"
         itemClass="w-full md:w-[calc(50%-25px)] lg:w-[calc(33.33%-40px)] max-w-[302px]"
-        content={organizationsContent} />
+        content={organizationsContent}
+      />
 
-      <OneColumn
-        bgImage
-        colClass="max-w-[603px]"
-        content={givingBackContent} />
+      <OneColumn bgImage colClass="max-w-[603px]" content={givingBackContent} />
 
       <OurMission />
     </>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
