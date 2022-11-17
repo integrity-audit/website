a2e982be7343be11d54c2d9918487dffbd1e2d9d
Kevin Hill <khill@integrity-audit.com>
2022-11-17 13:24:39 -0700

    UPDATED SITE AGAIN TODAY

--- content/Home/heroContent.js
+++ content/Home/heroContent.js
@@ -3,28 +3,15 @@ import Link from "next/link";
 export const heroContent = {
   content: (
     <>
-<<<<<<< HEAD
+
       <h1 className="title">The Integrity <span>Advantage</span></h1>
       <p className="sub-title">Joint venture audit and accounting experts backed by proprietary AI and machine learning solutions designed for energy accounting.</p>
       <Link href="#contact-us">
         <a className="btn-default">Contact Us</a>
       </Link>
-    </>,
+  
+    </>),
+  
   image: '/img-bg-hero-hires.jpg'
+  
 }
-=======
-      <h1 className="title">
-        The Integrity <span>Advantage</span>
-      </h1>
-      <p className="sub-title">
-        Experts backed by proprietary AI and machine learning solutions designed
-        for energy accounting
-      </p>
-      <Link href="#contact-us">
-        <a className="btn-default">Contact Us</a>
-      </Link>
-    </>
-  ),
-  image: "/img-bg-hero.jpg",
-};
->>>>>>> c4864dae170b8c5c1b83a0264aea6c663188ecea
