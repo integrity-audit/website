beb36bb1af2085285a83bed61c42ab6ef3800f8a
Kevin Hill <khill@integrity-audit.com>
2022-12-21 14:01:15 -0700

    Version 1.11

--- content/automation/aiq-video.js
+++ content/automation/aiq-video.js
@@ -5,9 +5,9 @@ export const expenditureAuditsContent2 = {
     <><p>
       <h2>Audit-iQ VIDEO</h2>      
  Watch the video below to see Audit-iQ in action.
- <div class="iframeVideo">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/azY9HL3YwIo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div>
+
+      <iframe src="https://www.youtube.com/embed/azY9HL3YwIo" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+
     
           
   
